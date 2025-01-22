7ed33ea8847eecfc8507d61cddc27bed628cb336
ChaiyoKung <kananek45@gmail.com>
2025-01-22 20:54:10 +0700

    feat: integrate MongoDB and dotenv for database access

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import "dotenv/config";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+const database = client.db("sample_mflix");
+const movies = database.collection("movies");
+
+const result = await movies.find().toArray();
+console.log(result);
+
+client.close();
